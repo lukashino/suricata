05294b4aaf08dca5e87e28699e972497ddadd129
Andrei Shchapaniak <shchapaniak.andrei@gmail.com>
2023-04-21 06:46:01 -0400

    change deprecated macros for DPDK 21.11+ compatibility

--- src/runmode-dpdk.c
+++ src/runmode-dpdk.c
@@ -1252,7 +1252,7 @@ static int DeviceValidateMTU(const DPDKIfaceConfig *iconf, const struct rte_eth_
 #if RTE_VERSION < RTE_VERSION_NUM(21, 11, 0, 0)
     // check if jumbo frames are set and are available
     if (iconf->mtu > RTE_ETHER_MAX_LEN &&
-            !(dev_info->rx_offload_capa & DEV_RX_OFFLOAD_JUMBO_FRAME)) {
+            !(dev_info->rx_offload_capa & RTE_ETH_RX_OFFLOAD_RSS_HASH)) {
         SCLogError("%s: jumbo frames not supported, set MTU to 1500", iconf->iface);
         SCReturnInt(-EINVAL);
     }
@@ -1268,7 +1268,7 @@ static void DeviceSetMTU(struct rte_eth_conf *port_conf, uint16_t mtu)
 #else
     port_conf->rxmode.max_rx_pkt_len = mtu;
     if (mtu > RTE_ETHER_MAX_LEN) {
-        port_conf->rxmode.offloads |= DEV_RX_OFFLOAD_JUMBO_FRAME;
+        port_conf->rxmode.offloads |= RTE_ETH_RX_OFFLOAD_RSS_HASH;
     }
 #endif
 }
